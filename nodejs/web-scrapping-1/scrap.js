6d19f7f17ea6cae710bd8a170c59de04397fc44e
behzadart <behzad.afsari@gmail.com>
2023-02-04 18:11:58 +0330

    complete web-scraping-1

--- nodejs/web-scrapping-1/scrap.js
+++ nodejs/web-scrapping-1/scrap.js
@@ -10,10 +10,25 @@ axios("https://www.manchestereveningnews.co.uk/sport/football/")
 .then(res=>{
     const htmlData = res.data;
     const $ = cheerio.load(htmlData)
-    console.log(htmlData)
+    // console.log(htmlData)
+    const articles = []
+
+    $(".teaser",htmlData).each((index,element)=>{
+        const title = $(element).children('.teaser-text').text()
+        const titleURL = $("element").children(".teaser-text").attr('href')
+        articles.push({
+            title,
+            titleURL
+        })
+        // console.log(title,titleURL)
+        // console.log(title)
+    })
+    console.log(articles)
 }).catch((err)=>{
     console.error(err)
 })
+
+console.log("+++++++++++++++++")
 
 
 
